f1c490a307911b9119f44088889d3df1a0caa64b
n0F4x <ggabor2002@gmail.com>
2024-10-26 11:31:52 +0200

    Basic logic tiles and positions

---
+++ HunJam24/Assets/Scripts/Logic/Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Logic
+{
+    class Position
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+
+        public int Distance(Position other)
+        {
+            return Math.Abs(X - other.X) + Math.Abs(Y - other.Y) + Math.Abs(Z - other.Z);
+        }
+    }
+}
---
+++ HunJam24/Assets/Scripts/Logic/Tile.cs
@@ -0,0 +1,17 @@
+﻿namespace Logic
+{
+    public class Tile
+    {
+        private Position _position;
+
+        public bool IsNextTo(Tile other)
+        {
+            return _position.Distance(other._position) == 1;
+        }
+
+        public bool IsOnSameLevel(Tile other)
+        {
+            return _position.Z == other._position.Z;
+        }
+    }
+}
