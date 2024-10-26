218955184b1d25c1216502b0013626d130ab0ddd
n0F4x <ggabor2002@gmail.com>
2024-10-26 23:11:26 +0200

    Fix StartTile

--- HunJam24/Assets/Scripts/Logic/Tiles/StartTile.cs
+++ HunJam24/Assets/Scripts/Logic/Tiles/StartTile.cs
@@ -1,14 +1,23 @@
 using Logic.Characters;
 using UnityEngine;
 
-namespace Logic.Tiles{
-    public class StartTile : TileBase {
+namespace Logic.Tiles
+{
+    public class StartTile : TileBase
+    {
+        public override bool AcceptsCharacter(Character character)
+        {
+            return true;
+        }
+
         public override bool AcceptCharacter(Character character)
         {
+            if (!AcceptsCharacter(character)) return false;
+
             Debug.Log("Happy End!");
             return true;
         }
-        
+
         public override void EnterFrom(Vector pos)
         {
             //Win game
