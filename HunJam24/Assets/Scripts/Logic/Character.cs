f00c2e91db542b6152baf83e9d33678811dafa47
n0F4x <ggabor2002@gmail.com>
2024-10-26 11:59:44 +0200

    Basic character move logic

---
+++ HunJam24/Assets/Scripts/Logic/Character.cs
@@ -0,0 +1,27 @@
+﻿namespace Logic
+{
+    public class Character
+    {
+        private MapManager _mapManager;
+        private Tile _tile;
+
+        public bool StepTo(Tile destination)
+        {
+            if (destination.IsOnSameLevel(_tile) && !_tile.IsNextTo(destination))
+                return false;
+
+            if (destination.IsOnNeighboringLevel(_tile) && destination.DistanceFrom(_tile) != 2)
+                return false;
+
+            // TODO: check for transparent tiles (door, pressure plate, ...)
+            if (_mapManager.Get(destination.Position + new Position(0, 0, 1)) != null)
+            {
+                return false;
+            }
+
+            _tile = destination;
+
+            return true;
+        }
+    }
+}
