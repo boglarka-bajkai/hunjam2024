8dd5156b0a78c121015231fbb37930b9af0df634
Czeglédi Krisztián <czegledi.krisz@gmail.com>
2024-10-26 14:48:40 +0200

    Added Movable tile

---
+++ HunJam24/Assets/Scripts/Logic/Tiles/MovableTile.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace Logic.Tiles{
+    public class MovableTile : Tile {
+        public bool Move() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
