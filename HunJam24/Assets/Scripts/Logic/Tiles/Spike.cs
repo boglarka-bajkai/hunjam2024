1d9bdd308e47b0b76a4e38c466178bd76921e93f
n0F4x <ggabor2002@gmail.com>
2024-10-27 11:15:42 +0100

    Half-ass started spikey

--- HunJam24/Assets/Scripts/Logic/Tiles/Spike.cs
+++ HunJam24/Assets/Scripts/Logic/Tiles/Spike.cs
@@ -13,5 +13,29 @@ namespace Logic.Tiles
         // public override Func<Character, bool> Command => character =>
         //     AcceptsCharacter(character) &&
         //     character.MoveOnto(MapManager.Instance.GetTilesAt(Position + new Vector(0, 0, -1)).First());
+
+        /*
+         * Checks whether the tile is able to accept a character.
+         * Acceptance means the character could be moved ONTO this tile.
+         * (Useful for doors, pressure plates, and other transparent objects...)
+         */
+        public override bool CanMoveOnFrom(Vector position) => false;
+
+        /*
+         * Checks whether the tile is able to accept another tile.
+         * Acceptance means the character could be moved ONTO this tile.
+         * (Useful for doors, pressure plates, and other transparent objects...)
+         */
+        public override bool CanMoveOn(TileBase tile) => false;
+
+        /*
+         * Checks whether the tile is able to accept a character.
+         * Acceptance means the character could be moved INTO this tile.
+         * (Useful for doors, pressure plates, and other transparent objects...)
+         */
+        public override bool CanMoveInFrom(Vector position)
+        {
+            return !_active && MapManager.Instance.GetTilesAt(Position).Count < 2;
+        }
     }
 }
