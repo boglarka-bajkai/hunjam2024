f3d07d86b72b1d518f3e3924c5ef1d824cf8d9ad
n0F4x <ggabor2002@gmail.com>
2024-10-27 12:43:24 +0100

    Ability to play reversed music

--- HunJam24/Assets/Scripts/Controls/AudioManager.cs
+++ HunJam24/Assets/Scripts/Controls/AudioManager.cs
@@ -19,6 +19,8 @@ namespace Controls
         [SerializeField]
         private AudioClip backgroundMusic;
 
+        [SerializeField] private AudioClip reversedBackgroundMusic;
+
         [SerializeField] public AudioClip step;
 
 
@@ -36,8 +38,18 @@ namespace Controls
             {
                 Debug.Log("[AudioManager::PlaySoundEffect]: Given sound effect is null");
             }
-            
+
             soundEffectsSource.PlayOneShot(soundEffect);
+        }
+
+        /*
+         * Starts playing the reversed background music if the normal background music was playing before
+         * Starts playing the normal background music if the reversed background music was playing before
+         */
+        public void PlayReversedMusic()
+        {
+            musicSource.clip = musicSource.clip == backgroundMusic ? reversedBackgroundMusic : backgroundMusic;
+            musicSource.Play();
         }
     }
 }
