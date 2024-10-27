4cf0d4d6910a1fa7ce864ca0575b2e54cae4234e
n0F4x <ggabor2002@gmail.com>
2024-10-27 12:18:14 +0100

    Adjustable background music

---
+++ HunJam24/Assets/Scripts/Controls/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Controls
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [Header("Audio Sources")]
+        // Header
+        [SerializeField]
+        private AudioSource musicSource;
+
+        [SerializeField] private AudioSource soundEffects;
+
+
+        [Header("Audio Clips")]
+        // Header
+        [SerializeField]
+        private AudioClip backgroundMusic;
+
+        [SerializeField] public AudioClip step;
+
+
+        private void Start()
+        {
+            if (!musicSource || !backgroundMusic) return;
+
+            musicSource.clip = backgroundMusic;
+            musicSource.Play();
+        }
+    }
+}
