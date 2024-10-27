3c2eca469456259d466faca400775117c0e18cdc
HariLevi <108951662+HariLevi@users.noreply.github.com>
2024-10-27 15:43:18 +0100

    Switch to dark on checkpoint

--- HunJam24/Assets/Scripts/MapLoader.cs
+++ HunJam24/Assets/Scripts/MapLoader.cs
@@ -28,33 +28,27 @@ public class MapLoader : MonoBehaviour
             { new Vector(2, 1, 0), "Base" },
             { new Vector(2, 0, 0), "Base" },
             { new Vector(1, 0, 0), "Base" },
-            { new Vector(3, 0, 0), "Base" },
             { new Vector(0, 0, 1), "Start" },
-            { new Vector(2, 0, 1), "Box" },
             { new Vector(2, 2, 1), "Checkpoint" }
         };
     Dictionary<Vector, string> Map2 =>
         new Dictionary<Vector, string>()
         {
-            { new Vector(1, 0, 0), "Base" },
-            { new Vector(1, 0, 1), "Checkpoint" },
-            { new Vector(1, 1, 0), "Base" },
-            { new Vector(1, 2, 0), "Base" }, //map.Add(new Vector(1, 2, 1), "Spike");
+            { new Vector(0, 2, 0), "Base" },
             { new Vector(0, 3, 0), "Base" },
-            { new Vector(1, 3, 0), "Base" },
+            { new Vector(0, 4, 0), "Base" },{ new Vector(0, 4, 1), "Start" },
+            { new Vector(1, 2, 0), "Base" },
+            { new Vector(1 ,4, 0), "Base" },
+            { new Vector(2, 0, 0), "Base" },
+            { new Vector(2, 1, 0), "Base" },
+            { new Vector(2, 2, 0), "Base" },
             { new Vector(2, 3, 0), "Base" },
-            { new Vector(0, 4, 0), "Base" },
-            { new Vector(1, 4, 0), "Base" }, //map.Add(new Vector(1, 2, 1), "Plate");
             { new Vector(2, 4, 0), "Base" },
-            { new Vector(0, 5, 0), "Base" },
-            { new Vector(1, 5, 0), "Base" },
-            { new Vector(1, 5, 1), "Box" },
-            { new Vector(2, 5, 0), "Base" },
-            { new Vector(0, 6, 0), "Base" },
-            { new Vector(1, 6, 0), "Base" },
-            { new Vector(2, 6, 0), "Base" },
-            { new Vector(1, 7, 0), "Base" },
-            { new Vector(1, 7, 1), "Start" }
+            { new Vector(3, 0, 0), "Base" },
+            { new Vector(3, 2, 0), "Base" },
+            { new Vector(4, 0, 0), "Base" },{ new Vector(4, 0, 1), "Checkpoint" },
+            { new Vector(4, 1, 0), "Base" },
+            { new Vector(4, 2, 0), "Base" }
         };
     Dictionary<Vector, string> Map3 =>
         new Dictionary<Vector, string>()
@@ -81,16 +75,43 @@ public class MapLoader : MonoBehaviour
             { new Vector(6, 3, 0), "Base" },
             { new Vector(6, 3, 1), "Checkpoint" }
         };
+    
+    Dictionary<Vector, string> Map4 =>
+        new Dictionary<Vector, string>()
+        {
+            { new Vector(1, 0, 0), "Base" },
+            { new Vector(1, 0, 1), "Checkpoint" },
+            { new Vector(1, 1, 0), "Base" },
+            { new Vector(1, 2, 0), "Base" }, //map.Add(new Vector(1, 2, 1), "Spike");
+            { new Vector(0, 3, 0), "Base" },
+            { new Vector(1, 3, 0), "Base" },
+            { new Vector(2, 3, 0), "Base" },
+            { new Vector(0, 4, 0), "Base" },
+            { new Vector(1, 4, 0), "Base" }, //map.Add(new Vector(1, 2, 1), "Plate");
+            { new Vector(2, 4, 0), "Base" },
+            { new Vector(0, 5, 0), "Base" },
+            { new Vector(1, 5, 0), "Base" },
+            { new Vector(1, 5, 1), "Box" },
+            { new Vector(2, 5, 0), "Base" },
+            { new Vector(0, 6, 0), "Base" },
+            { new Vector(1, 6, 0), "Base" },
+            { new Vector(2, 6, 0), "Base" },
+            { new Vector(1, 7, 0), "Base" },
+            { new Vector(1, 7, 1), "Start" }
+        };
 
     int currentMap = 0;
     List<Dictionary<Vector, string>> maps = new();
     public void Start() {
+        InvertColor.Instance.ToggleColorInversion();
         maps.Add(Map1);
         maps.Add(Map2);
         maps.Add(Map3);
+        maps.Add(Map4);
         TryLoadNextMap();
     }
     public void TryLoadNextMap() {
+        InvertColor.Instance.ToggleColorInversion();
         if (currentMap < maps.Count){
             MapManager.Instance.SetMap(maps[currentMap++]);
         }
