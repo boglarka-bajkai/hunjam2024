fc18420a0ced2c1cfa231ee2e6444d2a80beeb8f
n0F4x <ggabor2002@gmail.com>
2024-10-27 16:53:38 +0100

    Fix step sound on box pushing

--- HunJam24/Assets/Scripts/Logic/Characters/Character.cs
+++ HunJam24/Assets/Scripts/Logic/Characters/Character.cs
@@ -34,14 +34,15 @@ namespace Logic.Characters
 
                     if (targetTile == null)
                     {
-                        if (neighbour.CanMoveOnFrom(Position) 
-                            && CloneManager.Instance.GetClonesAt(neighbour.Position + new Vector(0, 0, 1)).Count <= 0) {
+                        if (neighbour.CanMoveOnFrom(Position)
+                            && CloneManager.Instance.GetClonesAt(neighbour.Position + new Vector(0, 0, 1)).Count <= 0)
+                        {
                             Debug.Log($"{neighbour.name} added from targetTile null");
                             result.Add(neighbour);
                         }
                     }
-                    else if (targetTile.TrueForAll(x=> x.CanMoveInFrom(Position))
-                        && CloneManager.Instance.GetClonesAt(neighbour.Position + new Vector(0, 0, 1)).Count <= 0)
+                    else if (targetTile.TrueForAll(x => x.CanMoveInFrom(Position))
+                             && CloneManager.Instance.GetClonesAt(neighbour.Position + new Vector(0, 0, 1)).Count <= 0)
                     {
                         result.AddRange(targetTile);
                         if (targetTile.TrueForAll(x => x is not MovableTile)) result.Add(neighbour);
@@ -68,7 +69,7 @@ namespace Logic.Characters
          */
         public bool MoveOnto(TileBase destination)
         {
-            var top = MapManager.Instance.GetTilesAt(destination.Position + new Vector(0,0,1));
+            var top = MapManager.Instance.GetTilesAt(destination.Position + new Vector(0, 0, 1));
             if (!ValidMoveOntoDestinations().Contains(destination))
             {
                 Debug.Log("moveonto early");
@@ -77,44 +78,50 @@ namespace Logic.Characters
 
             var tileOnNextTile =
                 MapManager.Instance.GetTilesAt(destination.Position + new Vector(0, 0, 1));
-            
-            if (tileOnNextTile != null && !tileOnNextTile.TrueForAll(x=> x.CanMoveInFrom(Position)))
+
+            if (tileOnNextTile != null && !tileOnNextTile.TrueForAll(x => x.CanMoveInFrom(Position)))
             {
                 return false;
             }
 
-			var dirVec = Position.DistanceFrom(destination.Position);
-			Debug.Log(dirVec);
-			Animator animator = GetComponent<Animator>();
-			if (dirVec.Equals(new Vector(1, 0, -1)))
-			{
-				Debug.Log("UR");
-				animator.SetInteger("dir", 0);
-			}
-			if (dirVec.Equals(new Vector(0, -1, -1)))
-			{
-				Debug.Log("UL");
-				animator.SetInteger("dir", 1);
-			}
-			if (dirVec.Equals(new Vector(-1, 0, -1)))
-			{
-				Debug.Log("DL");
-				animator.SetInteger("dir", 2);
-			}
-			if (dirVec.Equals(new Vector(0, 1, -1)))
-			{
-				Debug.Log("DR");
-				animator.SetInteger("dir", 3);
-			}
+            var dirVec = Position.DistanceFrom(destination.Position);
+            Debug.Log(dirVec);
+            Animator animator = GetComponent<Animator>();
+            if (dirVec.Equals(new Vector(1, 0, -1)))
+            {
+                Debug.Log("UR");
+                animator.SetInteger("dir", 0);
+            }
 
-			StartCoroutine(moveSoftlyTo(destination, top));
+            if (dirVec.Equals(new Vector(0, -1, -1)))
+            {
+                Debug.Log("UL");
+                animator.SetInteger("dir", 1);
+            }
+
+            if (dirVec.Equals(new Vector(-1, 0, -1)))
+            {
+                Debug.Log("DL");
+                animator.SetInteger("dir", 2);
+            }
+
+            if (dirVec.Equals(new Vector(0, 1, -1)))
+            {
+                Debug.Log("DR");
+                animator.SetInteger("dir", 3);
+            }
+
+            StartCoroutine(moveSoftlyTo(destination, top));
 
             return true;
         }
+
         const float WAITBEFORESTART = .1f;
         const float MOVE_MULTIPLIER = 1.1f;
         bool pushing = false;
-        IEnumerator moveSoftlyTo(TileBase destination, List<TileBase> top) {
+
+        IEnumerator moveSoftlyTo(TileBase destination, List<TileBase> top)
+        {
             movingCount++;
             if (this is Player) MapManager.Instance.ResetTiles();
             if (this is Player) CloneManager.Instance.Tick();
@@ -123,53 +130,69 @@ namespace Logic.Characters
             yield return new WaitForEndOfFrame();
             Animator animator = GetComponent<Animator>();
             string animName = "";
-            switch(animator.GetInteger("dir")){
-                case 0: animName = pushing ? "player_push_UR" :"player_jump_UR"; break;
-                case 1: animName = pushing ? "player_push_UL" :"player_jump_UL"; break;
-                case 2: animName = pushing ? "player_push_DL" :"player_jump_DL"; break;
-                case 3: animName = pushing ? "player_push_DR" :"player_jump_DR"; break;
+            switch (animator.GetInteger("dir"))
+            {
+                case 0:
+                    animName = pushing ? "player_push_UR" : "player_jump_UR";
+                    break;
+                case 1:
+                    animName = pushing ? "player_push_UL" : "player_jump_UL";
+                    break;
+                case 2:
+                    animName = pushing ? "player_push_DL" : "player_jump_DL";
+                    break;
+                case 3:
+                    animName = pushing ? "player_push_DR" : "player_jump_DR";
+                    break;
                 default: break;
             }
-            if (animName != ""){
+
+            if (animName != "")
+            {
                 animator.Play(animName);
             }
 
-            while (t <= WAITBEFORESTART) {
+            while (t <= WAITBEFORESTART)
+            {
                 t += Time.deltaTime;
                 yield return new WaitForEndOfFrame();
             }
+
             //Move smoothly
             t = 0f;
-            Vector destinationV = destination.Position + new Vector(0,0,1);
+            Vector destinationV = destination.Position + new Vector(0, 0, 1);
             Vector3 startPos = transform.position;
             Debug.Log($"started from {Position.ToString()}");
-            if (destinationV.Order > Position.Order) 
+            if (destinationV.Order > Position.Order)
                 GetComponent<SpriteRenderer>().sortingOrder = destinationV.Order;
-            AudioManager.Instance.PlaySoundEffect("Step");
-            while (t <= 1f) {
+            if (!pushing) AudioManager.Instance.PlaySoundEffect("Step");
+            while (t <= 1f)
+            {
                 t += Time.deltaTime * MOVE_MULTIPLIER;
                 transform.position = Vector3.Lerp(startPos, destinationV.UnityVector, t);
                 yield return new WaitForEndOfFrame();
             }
+
             Tile = destination;
             transform.position = Position.UnityVector;
             GetComponent<SpriteRenderer>().sortingOrder = Position.Order;
-            
-            if (top != null) top.ForEach(x=> x.EnterFrom(Position));
-			if (this is Player p)
-			{
+
+            if (top != null) top.ForEach(x => x.EnterFrom(Position));
+            if (this is Player p)
+            {
                 MapManager.Instance.PlayerMoved(Tile);
-            
-				if(p.ShouldBeDead)
-				{
-					OverlayManager.Instance.ShowLoseScreen();
-				}
-			}
+
+                if (p.ShouldBeDead)
+                {
+                    OverlayManager.Instance.ShowLoseScreen();
+                }
+            }
+
             movingCount--;
             pushing = false;
         }
 
-        
+
         public bool Push(MovableTile movableTile)
         {
             Debug.Log("push");
@@ -181,11 +204,11 @@ namespace Logic.Characters
             }
 
             var destination = movableTile.Position + distance;
-            var ground = MapManager.Instance.GetTilesAt(destination + new Vector(0,0,-1));
+            var ground = MapManager.Instance.GetTilesAt(destination + new Vector(0, 0, -1));
             Debug.Log("asd");
-            Debug.Log($"zugugt{(destination + new Vector(0,0,-1)).ToString()}");
+            Debug.Log($"zugugt{(destination + new Vector(0, 0, -1)).ToString()}");
             if (ground == null) Debug.Log("null");
-            if (ground == null || !ground.TrueForAll(x=> x.CanMoveOn(movableTile))) return false;
+            if (ground == null || !ground.TrueForAll(x => x.CanMoveOn(movableTile))) return false;
             Animator animator = GetComponent<Animator>();
             animator.SetTrigger("pushing");
             pushing = true;
